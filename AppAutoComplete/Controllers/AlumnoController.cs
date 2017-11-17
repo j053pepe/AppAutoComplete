9f7c7f25a8e56028b9b803559b930a2abd0b59e0
j053pepe <j053_pepe@outlook.com>
2017-11-17 01:22:22 -0600

    Agregar archivos de proyecto.

---
+++ AppAutoComplete/Controllers/AlumnoController.cs
@@ -0,0 +1,29 @@
+﻿using AppAutoComplete.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace AppAutoComplete.Controllers
+{
+    public class AlumnoController : ApiController
+    {
+        private IeseEncuestaEntities db = new IeseEncuestaEntities();
+
+        [HttpGet]
+        [ActionName("Nombre")]
+        public IHttpActionResult GetNombre(string filtro)
+        {
+            return Ok(
+            db.Alumno.Where(al => al.Nombre.Contains(filtro))
+                        .Select(al => new
+                        {
+                            Value = al.AlumnoId,
+                            Text = al.Nombre + " " + al.Paterno + " " + al.Materno
+                        }).ToList()
+            );
+        }
+    }
+}
---
+++ AppAutoComplete/index.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    var indexFn = {
+        init: function () {
+            $('#txtfiltro').on('keyup', indexFn.txtBind);
+        },
+        txtBind: function () {
+            if ($(this).val().length > 2) {
+                indexFn.Alumnos($(this).val());
+            }
+        },
+        Alumnos: function (filtro) {
+            $.ajax({
+                type: "Get",
+                url: "Api/Alumno/nombre",
+                data: { "filtro": filtro },
+                contentType: "application/json; charset=utf-8"
+            }).done(function (data) {
+                horsey($('#txtfiltro')[0], {
+                    source: [{
+                        list: data
+                    }],
+                    getText: 'Text',
+                    getValue: 'Value',
+                    limit: 2
+                });
+            });
+        }
+    };
+
+    indexFn.init();
+});
